42a1c8fd58ba13140e0587596a0e3f4ea221ec7c
Max Rodrigues <maxuel.rodrigues@gmail.com>
2024-05-02 16:11:04 -0300

    add controller, views and routes

---
+++ resources/views/regular-users.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        @vite('resources/css/app.css')
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <div class="h-screen">
+            <div class="text-white text-6xl text-center pt-20">Regular Users</div>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('regular-users', function () {
+    return view('regular-users');
+});
+
+Route::get('admin-users', function () {
+    return view('admin-users');
+});
+
+Route::get('feature-flags', \App\Http\Controllers\FeatureFlagsController::class);
